a5e31cc752a0ec9ab6aa4122b2093eb598e6fd5d
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-07-12 13:56:21 +0530

    added sockets

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -8,7 +8,6 @@ app.use(express.json());
 
 
 const server = createServer(app);
-
 const io = new Server(server,{
     cors : {
          origin: "http://localhost:5173",
@@ -18,9 +17,31 @@ const io = new Server(server,{
 
 io.on('connection',(socket)=>{
     console.log("a user connected");
-    socket.on("create-room",(createRoom)=>{
-        console.log(createRoom)
-    })
+    socket.on("create-room", ({ user }) => {
+    socket.join(user.roomId);
+    console.log("user created a room",user.roomId,user)
+    socket.to(user.roomId).emit("create-room", {
+      user
+    });
+  });
+
+  socket.on("join-room", ({  user }) => {
+  
+    console.log("user joined:",user);
+    socket.to(user.roomId).emit("user-joined", { user });
+  });
+
+  socket.on("offer",(data)=>{
+    socket.to(data.roomId).emit("offer",data);
+  })
+
+  socket.on("answer",(data)=>{
+    socket.to(data.roomId).emit("answer",data);
+  })
+
+  socket.on("ice-candidate",(data)=>{
+    socket.to(data.roomId).emit("ice-candidate",data);
+  })
 })
 
 server.listen(3000,()=>{
